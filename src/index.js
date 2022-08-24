a757e3d3178192e36f7d5b7dd6871e68e62562a0
NicKyeaa <tin.jurcevic@gmail.com>
2022-08-24 22:01:05 +0200

    Added schema for equipment

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// Imports
+require('./db/mongoose.js')
+const fastify = require('fastify')({ logger: true })
+
+fastify.route({
+    method: 'GET',
+    url: '/equipment',
+    schema: {
+        // request needs to have a querystring with a `name` parameter
+        querystring: {
+            name: { type: 'string' }
+        },
+        // the response needs to be an object with an `hello` property of type 'string'
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    hello: { type: 'string' }
+                }
+            }
+        }
+    },
+    // this function is executed for every request before the handler is executed
+    preHandler: async(request, reply) => {
+        // E.g. check authentication
+    },
+    handler: async(request, reply) => {
+        return { hello: 'world' }
+    }
+})
+
+const start = async() => {
+    try {
+        await fastify.listen({ port: 3500 })
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+start()
---
+++ src/models/equipment.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Equipment = mongoose.model('Equipment', {
+    category: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    type: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    model: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    serialNumber: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    inventoryNumber: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    location: {
+        type: String,
+        required: false,
+        trim: true
+    },
+});
